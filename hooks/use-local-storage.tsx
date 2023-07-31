646daa8f31f5f7e0576c8ced02dc458ced100873
Ilya Dubinevich <bucks20007@gmail.com>
2023-08-01 00:02:27 +0300

    Fix localStorage

--- hooks/use-local-storage.tsx
+++ hooks/use-local-storage.tsx
@@ -5,10 +5,10 @@ const useLocalStorage = (key: string, initialValue: any) => {
 		// Initialize the state
 		try {
 			if (typeof window !== 'undefined') {
-				const value = window.localStorage.getItem(key)
-				// Check if the local storage already has any values,
-				// otherwise initialize it with the passed initialValue
-				return value ? JSON.parse(value) : initialValue
+				const value = localStorage.getItem(key)
+			// Check if the local storage already has any values,
+			// otherwise initialize it with the passed initialValue
+			return value ? JSON.parse(value) : initialValue
 			}
 		} catch (error) {
 			console.log(error)
@@ -20,9 +20,9 @@ const useLocalStorage = (key: string, initialValue: any) => {
 			// If the passed value is a callback function,
 			//
 			if (typeof window !== 'undefined') {
-				const valueToStore = value instanceof Function ? value(state) : value
-				window.localStorage.setItem(key, JSON.stringify(valueToStore))
-				setState(value)
+			const valueToStore = value instanceof Function ? value(state) : value
+				localStorage.setItem(key, JSON.stringify(valueToStore))
+			setState(value)
 			}
 		} catch (error) {
 			console.log(error)
