dea0d08aa70fa8568d87f83f2f07699e0d163c4f
Ilya Dubinevich <bucks20007@gmail.com>
2023-08-15 15:10:11 +0300

    Добавление метрики

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import App, { AppContext, AppInitialProps, AppProps } from 'next/app'
 import Header from '../components/core/header/Header'
 import { Bellota } from 'next/font/google'
 import Footer from '../components/core/footer/Footer'
+import Head from 'next/head'
 export const bellota = Bellota({ subsets: ['cyrillic'], weight: ['400', '700'] })
 
 type AppPropsWithLayout = AppProps & {
@@ -15,6 +16,12 @@ type AppPropsWithLayout = AppProps & {
 function MyApp({ Component, pageProps, headerApi }: AppPropsWithLayout) {
   return (
       <div className={bellota.className}>
+          <Head>
+              <title>Доставка горячего шашлыка, вкусной еды во Владимире | Аджика.</title>
+              <meta name="yandex-verification" content="eaf7646179b9c4af" />
+              <meta name="google-site-verification" content="vNeViD2l3TdKMFVj6wbz0fAq5rpupMLiRmjnobU9NJE" />
+              <meta name="description" content="Блюда на мангале из свежей, фермерской свинины, баранины, говядины, курицы и овощей! Быстрая доставка с пылу-жару с 12.00 до 22.00! Самовывоз с ул. Горького, 40" />
+          </Head>
           <Header {...headerApi} />
           <Component {...pageProps} />
           <Footer {...headerApi} />
--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -137,6 +137,7 @@ const Cart: FC = () => {
 			text: message
 		})
 			.then(res => {
+				ym(94630142,'reachGoal','order')
 				setToast(true)
 				clearAll()
 				const replace = setTimeout(() => {
@@ -199,160 +200,160 @@ const Cart: FC = () => {
 	const inputStyles = 'bg-transparent border border-gray-600 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
 
 	return (
-		<div className={style.cart}>
-			{
-				spinner && <div className={spinner ? style.spinnerBack : ''}>
-					<div role="status">
-						<svg aria-hidden="true"
-							 className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-							 viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path
-								d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-								fill="currentColor"/>
-							<path
-								d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-								fill="currentFill"/>
-						</svg>
-						<span className="sr-only">Loading...</span>
-					</div>
-				</div>
-			}
+			<div className={style.cart}>
+				{
+					spinner && <div className={spinner ? style.spinnerBack : ''}>
+										<div role="status">
+											<svg aria-hidden="true"
+													 className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+													 viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+												<path
+													d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+													fill="currentColor"/>
+												<path
+													d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+													fill="currentFill"/>
+											</svg>
+											<span className="sr-only">Loading...</span>
+										</div>
+									</div>
+				}
 
-			{toast && <div className={style.toast}>
-				<div className={style.toast_body}>
-				  	<div className={style.toast_close} onClick={onClose}>
-						<Image src="/images/close.svg" width={25} height={25} alt="Закрыть"></Image>
-					</div>
-					<div className={style.toast_title + ' ' + philosopher.className}>Спасибо за заказ</div>
-				  	<div className={style.toast_svg}>
-					 	<Image src="/images/spinner.svg" width={150} height={150} alt="знак загрузки" />
-					</div>
-					<div className={style.toast_text}>Мы уже начали готовить вашу еду <span className={style.toast_heart}>♡</span></div>
-				</div>
-			</div>}
+				{toast && <div className={style.toast}>
+									<div className={style.toast_body}>
+										<div className={style.toast_close} onClick={onClose}>
+											<Image src="/images/close.svg" width={25} height={25} alt="Закрыть"></Image>
+										</div>
+										<div className={style.toast_title + ' ' + philosopher.className}>Спасибо за заказ</div>
+										<div className={style.toast_svg}>
+											<Image src="/images/spinner.svg" width={150} height={150} alt="знак загрузки" />
+										</div>
+										<div className={style.toast_text}>Мы уже начали готовить вашу еду <span className={style.toast_heart}>♡</span></div>
+									</div>
+								</div>}
 
-			<div className="cart__container">
-				<h2 className={style.title + ' mb-5 ' + philosopher.className}>Заказ</h2>
-				<div className="border-b opacity-10 mb-5"></div>
+				<div className="cart__container">
+					<h2 className={style.title + ' mb-5 ' + philosopher.className}>Заказ</h2>
+					<div className="border-b opacity-10 mb-5"></div>
 
-				{selectProduct.length === 0 && <div className={style.empty}>
-					<div className={style.empty_text}>Корзина пуста...</div>
-					<Link
-					  href="/#cards"
-					  className={style.at_home + ' text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2'}
-					>Оформить заказ</Link>
-				</div>}
+					{selectProduct.length === 0 && <div className={style.empty}>
+											<div className={style.empty_text}>Корзина пуста...</div>
+											<Link
+												href="/#cards"
+												className={style.at_home + ' text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2'}
+											>Оформить заказ</Link>
+										</div>}
 
-				{selectProduct.length !== 0 && <>
-					<CardsInCart
-					  callbackRemove={callbackRemove}
-					  callbackIncreaseDecrease={callbackIncreaseDecrease}
-					  selectedProducts={selectProduct}
-					  counter={counter}
-					  count={count}
-					  uniqSelectedProducts={uniqSelectedProducts}
-					  className="mb-10"
-					/>
+					{selectProduct.length !== 0 && <>
+											<CardsInCart
+												callbackRemove={callbackRemove}
+												callbackIncreaseDecrease={callbackIncreaseDecrease}
+												selectedProducts={selectProduct}
+												counter={counter}
+												count={count}
+												uniqSelectedProducts={uniqSelectedProducts}
+												className="mb-10"
+											/>
 
-					<div className={style.total}>
-						<div className={style.total_wrap + ' mb-10'}>
-							<div className="d">Итого:</div>
-							<div className={style.between}></div>
-							<div className={style.sum + ' ' + philosopher.className}>{sum} руб.</div>
-						</div>
+											<div className={style.total}>
+												<div className={style.total_wrap + ' mb-10'}>
+													<div className="d">Итого:</div>
+													<div className={style.between}></div>
+													<div className={style.sum + ' ' + philosopher.className}>{sum} руб.</div>
+												</div>
 
-						<div className={style.total_wrap + ' mb-5'}>
-							<div className="d">Доставка:</div>
-							<div className={style.between}></div>
-							<div className={style.sum + ' ' + philosopher.className}>150 руб.</div>
-						</div>
-					</div>
+												<div className={style.total_wrap + ' mb-5'}>
+													<div className="d">Доставка:</div>
+													<div className={style.between}></div>
+													<div className={style.sum + ' ' + philosopher.className}>150 руб.</div>
+												</div>
+											</div>
 
 					<form onSubmit={handleSubmit} className={style.form} >
 
-					  <div className={style.wrap}>
-						<div className="relative w-full mb-10">
-							<label
-								htmlFor="first_name"
-								className={philosopher.className + ' ' + style.label}
-							>Ваше имя:</label>
-							<input
-								type="text"
-								id="first_name"
-								className={inputStyles + ' ' + style.label}
-								value={name}
-								onInput={changeNameField}
-								required
-							/>
-						</div>
+												<div className={style.wrap}>
+													<div className="relative w-full mb-10">
+														<label
+															htmlFor="first_name"
+															className={philosopher.className + ' ' + style.label}
+														>Ваше имя:</label>
+														<input
+															type="text"
+															id="first_name"
+															className={inputStyles + ' ' + style.label}
+															value={name}
+															onInput={changeNameField}
+															required
+														/>
+													</div>
 
-						<div className="relative w-full mb-10">
-							<label htmlFor="phone" className={philosopher.className + ' ' + style.label}>Ваш телефон:</label>
-							<Input
-								className={inputStyles + ' ' + style.label}
-								minLength="16"
-								maxLength="16"
-								required
-								country="RU"
-								international
-								withCountryCallingCode
-								value={phone}
-								onChange={(value: string) => {
-								setPhone(value)
-								setPhoneValidate(false)
-								}}
-							/>
-								{phoneValidate && <div className={style.error}>Кажется здесь не хватает цифр телефона</div>}
-						</div>
+													<div className="relative w-full mb-10">
+														<label htmlFor="phone" className={philosopher.className + ' ' + style.label}>Ваш телефон:</label>
+														<Input
+															className={inputStyles + ' ' + style.label}
+															minLength="16"
+															maxLength="16"
+															required
+															country="RU"
+															international
+															withCountryCallingCode
+															value={phone}
+															onChange={(value: string) => {
+								  setPhone(value)
+								  setPhoneValidate(false)
+							  }}
+														/>
+							  {phoneValidate && <div className={style.error}>Кажется здесь не хватает цифр телефона</div>}
+													</div>
 
-						<label htmlFor="simple-search"className={philosopher.className + ' ' + style.label}>Доставить по адресу:</label>
+													<label htmlFor="simple-search"className={philosopher.className + ' ' + style.label}>Доставить по адресу:</label>
 
-						<div className="relative w-full mb-10">
-							<div
-								className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"></div>
-							<input
-								type="text"
-								id="simple-search"
-								className={inputStyles + ' ' + style.label}
-								required
-								value={address}
-								placeholder="Название улицы..."
-								onInput={searchAddresses}
-							/>
+													<div className="relative w-full mb-10">
+														<div
+															className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"></div>
+														<input
+															type="text"
+															id="simple-search"
+															className={inputStyles + ' ' + style.label}
+															required
+															value={address}
+															placeholder="Название улицы..."
+															onInput={searchAddresses}
+														/>
 
-							{listAddresses.length !== 0 && <div
-								className="absolute z-10 w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-								{listAddresses.map((address, idx) =>
-									<a
-										key={address.value}
-										onClick={() => checkAddress(address.value)}
-										className={'block w-full px-4 py-2 text-white bg-blue-700 border-b border-gray-200 cursor-pointer dark:bg-gray-800 dark:border-gray-600 ' + (idx === 0 ? 'rounded-t-lg ' : ' ') + (idx === (listAddresses.length - 1) ? 'rounded-b-lg ' : '')}
-									>{address.value}</a>
-								)}
-							</div>}
+							  {listAddresses.length !== 0 && <div
+																className="absolute z-10 w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+								  {listAddresses.map((address, idx) =>
+									  <a
+										  key={address.value}
+										  onClick={() => checkAddress(address.value)}
+										  className={'block w-full px-4 py-2 text-white bg-blue-700 border-b border-gray-200 cursor-pointer dark:bg-gray-800 dark:border-gray-600 ' + (idx === 0 ? 'rounded-t-lg ' : ' ') + (idx === (listAddresses.length - 1) ? 'rounded-b-lg ' : '')}
+									  >{address.value}</a>
+								  )}
+															</div>}
 
-						</div>
+													</div>
 
-						<button
-							type="submit"
-							className={'text-white mb-5 bg-blue-400 dark:bg-blue-500 font-medium rounded-lg px-5 py-2.5 text-center ' + style.label + ' ' + style.send_btn}
-						>Оформить заказ</button>
-						<ul className={style.notes}>
-							<li>Доставка осуществляется с 12 до 22 (мск).</li>
-							<li>От 1500 руб. бесплатно, иначе оплачивается курьеру Яндекс самостоятельно.</li>
-							<li>Самовывоз - бесплатно.</li>
-						</ul>
-					  </div>
+													<button
+														type="submit"
+														className={'text-white mb-5 bg-blue-400 dark:bg-blue-500 font-medium rounded-lg px-5 py-2.5 text-center ' + style.label + ' ' + style.send_btn}
+													>Оформить заказ</button>
+													<ul className={style.notes}>
+														<li>Доставка осуществляется с 12 до 22 (мск).</li>
+														<li>От 1500 руб. бесплатно, иначе оплачивается курьеру Яндекс самостоятельно.</li>
+														<li>Самовывоз - бесплатно.</li>
+													</ul>
+												</div>
 
-					  <div className={philosopher.className + ' ' + style.stock}>
-						При заказе от
-						1500 руб. морс в подарок
-					  </div>
-					</form>
+												<div className={philosopher.className + ' ' + style.stock}>
+													При заказе от
+													1500 руб. морс в подарок
+												</div>
+											</form>
 
-				</>}
+										</>}
+				</div>
 			</div>
-		</div>
 	)
 }
 
